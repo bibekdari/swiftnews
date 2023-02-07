37a549694a6970e126dfc4de0614656920ea79a7
Bibek <timalsina.bibek@gmail.com>
2023-02-07 22:49:38 +0900

    Added a button in viewDidLoad with an action on click.

--- UIButtonConfiguration/UIButtonConfiguration/ViewController.swift
+++ UIButtonConfiguration/UIButtonConfiguration/ViewController.swift
@@ -11,6 +11,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .tintColor
+        
+        button.setTitle("Parrot", for: .normal)
+        button.addAction(.init(handler: { action in
+            print("I am parrot")
+        }), for: .touchUpInside)
+        
+        view.addSubview(button)
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 
 }
